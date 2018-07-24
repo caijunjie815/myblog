e5301368ce07653d3064b7e40bff4917ce86964c
eddy <cjj1992815@gmail.com>
2018-07-25 03:05:59 +1000

    nothing.

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', PostList.as_view(), name='index'),
     path('category/<int:category>', CategoryList.as_view(), name='category'),
-    # name is mapping this url, which can be used in templates
-    # <int:category> is the keywords matching self.kwargs['category'] in views.py
     path('search/', Search.as_view(), name='search'),
     path('article/<int:pk>/', PostView.as_view(), name='article'),
     path('article/<int:article_id>/comment/', blog_views.post_comment, name='comment'),
